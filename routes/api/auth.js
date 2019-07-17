f75b4f5be81b7461ae5eecb0bedeed2a67ec5f7e
yinfeiw1998 <36552067+yinfeiw1998@users.noreply.github.com>
2019-07-17 14:12:36 -0700

    change the auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -6,51 +6,54 @@ const User = require('./model_auth/user')
 
 // Backend code goes here
 
-router.post("/Register",async(req,res) => {
+router.post("/Register",async (req,res) => {
     const {userName, email, password1, password2} = req.body
     console.log(req.body);
+    //check if password 1 and 2 matches
     if (password1 === password2){
+        
         const user = new User({userName: userName, email: email, password: password1})
-        User.findOne({userName: user.userName}).then((sameNameUsers) => {
+        try{
+            //check if the user has already been registered
+            const sameNameUsers = await User.findOne({userName: user.userName})
             if (!sameNameUsers){
-                User.findOne({email: user.email}).then((sameEmailUsers) => {
-                    if (!sameEmailUsers){
-                        user.save().then(() => {
-                            res.status(200).send(user)
-                        })
+                try{
+                    //chcek if the email address has already been registered
+                    const sameEmailUsers = await User.findOne({email: user.email})
+                    if(!sameEmailUsers){
+                        //save the user into db
+                        await user.save()
+                        res.status(200).send(user)
                     }else{
                         res.status(400).send("The email address is already exist! Please try another one! Or ask ZHUXIAOWEN for a new one")
                     }
-                })
+
+                }catch(error){
+                    res.status(500).send(e)
+                }
             }else{
                 res.status(400).send("The username is already exist! Please try another one! Or ask ZHUXIAOWEN for a new name")
             }
-        }).catch((e) => {
+
+        }catch(e){
             res.status(500).send(e)
-        })
+        }
+
     }else{
         res.status(400).send("The password one and two does not match, please check them again! Or ask ZHUXIAOWEN for a new one")
     }
 
-    
 })
 
-router.post("/Login",async(req,res) => {
-
-    User.findOne({email: req.body.email}).then((onRecordUser) => {
-        if (!onRecordUser){
-            res.status(400).send("The email address does not match any on the record! please try again! ")
-        }else if(onRecordUser.password !== req.body.password){
-            res.status(400).send("The password is not correct! please try again! ")
-        }else{
-            res.status(200).send(onRecordUser)
-        }
-    }).catch((e) => {
-        res.status(500).send(e)
-    })
+router.post("/Login",async (req,res) => {
+    try{
+        //check if the user has a valid email address and the password matched
+        const user = await User.findbyCredentials(req.body.email, req.body.password)
+        res.status(200).send(user)
+    }catch(e){
+        res.status(400).send()
+    }
 })
-
-
 
 // end of backend code
 
