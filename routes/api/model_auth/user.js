68dbafc163b97091704fc9d056e91dc86289136e
yinfeiw1998 <36552067+yinfeiw1998@users.noreply.github.com>
2019-07-17 14:13:33 -0700

    change the user model

--- routes/api/model_auth/user.js
+++ routes/api/model_auth/user.js
@@ -1,20 +1,21 @@
 const validator = require('validator')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
-
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     userName: {
         type: String,
         required: true,
         trim: true,
         validate(value){
             if (value.length<5){
-                throw new Error('Username\'s length should longer than 5')
+                throw new Error('Username\'s length should no shorter than 5')
             }
         }
     },
     email:{
         type: String,
+        unique: true,
         required: true,
         trim: true,
         lowercase: true,
@@ -29,5 +30,34 @@ const User = mongoose.model('User', {
         required: true,
     }
 })
+
+
+userSchema.statics.findbyCredentials = async (email, password) =>{
+    const user = await User.findOne({email: email})
+    if(!user){
+        throw new Error("Cannot find the email address")
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch){
+        throw new Error("Password is not match")
+    }
+
+    return user
+}
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
+
 
 module.exports = User
