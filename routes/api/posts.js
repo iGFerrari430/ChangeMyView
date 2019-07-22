4dd497e0f2e522d35d9893d7c14cf3a89f4749db
Wang Yinfei <yinfeiw@uci.edu>
2019-07-22 15:42:27 -0700

    change a little attached to last change

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,19 +4,17 @@ const Topic = require("./models/topic")
 const Proposition = require("./models/proposition")
 // backend code starts here 
 
-router.get("/Get",async (req,res) => {
-
+router.get("/Get/allTopics",async (req,res) => {
 })
 
 
-router.post("/Post", async (req, res) => {
+router.post("/Post/allTopics", async (req, res) => {
     const {rich_tc, plain_tc, title, userName, postDate, proposition1, proposition2} = req.body
 
     const prop1 = new Proposition({userName: userName, postDate: postDate, content: proposition1})
     const prop2 = new Proposition({userName: userName, postDate: postDate, content: proposition2})
     const topic = new Topic({richTextContent: rich_tc, plainTextContent: plain_tc, title: title, userName: userName, postDate: postDate, proposition: [prop1, prop2]})
     try{
-        console.log("get here")
         await prop1.save()
         await prop2.save()
         await topic.save()
